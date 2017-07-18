92882040eea43c1f4da2e2ea2e57808413c1213f
anne <richardson.ae@gmail.com>
2017-07-18 15:42:06 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    var $inputField = $( 'input' ).attr('name', 'new-items');
+    var $mainList = $( '#main-list' );
+    var $listItems = $mainList.children();
+
+
+
+    $inputField.on('keyup', function (e) {
+      if (e.keyCode == 13) {
+        $mainList.append('<li>'+$(this).val()+' <i class="material-icons">highlight_off</i></li>');
+        $inputField.val('');
+      }
+    });
+
+    // click to cross off each item
+    $( '#main-list' ).on("click", 'li', function(){
+      $(this).toggleClass('complete');
+    });
+
+    // add 'delete' icon functionality
+    $( '#main-list' ).on("click", '.material-icons', function(){
+      $(this).parent().remove();
+    });
+
+
+});

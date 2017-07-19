23733fe41d1b4c30bef26101b24a378d23e1957b
anne <richardson.ae@gmail.com>
2017-07-18 19:48:56 -0500

    Add checkboxes to items and replace toggle functionality to accommodate

--- scripts.js
+++ scripts.js
@@ -7,18 +7,32 @@ $( document ).ready(function() {
     // makeshift onEnter listener
     $inputField.on('keyup', function (e) {
       if (e.keyCode == 13) {
-        $mainList.prepend('<li>'+$(this).val()+' <i class="material-icons">highlight_off</i></li>');
+        $mainList.prepend('<li class="incomplete"><i class="material-icons check-boxes">check_box_outline_blank</i> '+$(this).val()+' <i class="material-icons delete">highlight_off</i></li>');
         $inputField.val('');
       }
     });
 
     // click to cross off each item
-    $( '#main-list' ).on("click", 'li', function(){
-      $(this).toggleClass('complete');
+    // $( '#main-list' ).on("click", 'li', function(){
+    //   $(this).toggleClass('complete');
+    // });
+
+    // replace toggle with functionality to include icons
+    $( '#main-list' ).on("click", '.incomplete', function(){
+      var $unchecked = $(this).children('i').first().html('check_box_outline_blank');
+      $unchecked.html('check_box');
+      $(this).addClass('complete').removeClass('incomplete');
+    });
+
+    $( '#main-list' ).on("click", '.complete', function(){
+      var $checked = $(this).children('i').first().html('check_box');
+      $checked.html('check_box_outline_blank');
+      $(this).addClass('incomplete').removeClass('complete');
     });
 
     // add 'delete' icon functionality
-    $( '#main-list' ).on("click", '.material-icons', function(){
+    $( '#main-list' ).on("click", '.delete', function(){
+      confirm("Are you sure you want to delete this item?");
       $(this).parent().remove();
     });
 

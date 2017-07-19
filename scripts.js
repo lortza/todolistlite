26aa2042c790a2aa2d46792da4b320be77d524c5
anne <richardson.ae@gmail.com>
2017-07-19 08:46:46 -0500

    Resolve #3 delete icon positioning

--- scripts.js
+++ scripts.js
@@ -3,11 +3,12 @@ $( document ).ready(function() {
     var $inputField = $( 'input' ).attr('name', 'new-items');
     var $mainList = $( '#main-list' );
     var $listItems = $mainList.children();
+    var $listItemTemplate = $('<li class="incomplete"><i class="material-icons delete">highlight_off</i><i class="material-icons check-boxes">check_box_outline_blank</i></li>');
 
     // makeshift onEnter listener
     $inputField.on('keyup', function (e) {
       if (e.keyCode == 13) {
-        $mainList.prepend('<li class="incomplete"><i class="material-icons check-boxes">check_box_outline_blank</i> '+$(this).val()+' <i class="material-icons delete">highlight_off</i></li>');
+        $mainList.prepend($listItemTemplate.append( $(this).val() ) );
         $inputField.val('');
       }
     });
@@ -19,13 +20,13 @@ $( document ).ready(function() {
 
     // replace toggle with functionality to include icons
     $( '#main-list' ).on("click", '.incomplete', function(){
-      var $unchecked = $(this).children('i').first().html('check_box_outline_blank');
+      var $unchecked = $(this).children('i').last().html('check_box_outline_blank');
       $unchecked.html('check_box');
       $(this).addClass('complete').removeClass('incomplete');
     });
 
     $( '#main-list' ).on("click", '.complete', function(){
-      var $checked = $(this).children('i').first().html('check_box');
+      var $checked = $(this).children('i').last().html('check_box');
       $checked.html('check_box_outline_blank');
       $(this).addClass('incomplete').removeClass('complete');
     });
